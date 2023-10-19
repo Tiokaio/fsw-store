3e75cd9773dff4d324f48ac6d2624f31923d6882
Kaio Vieira <kaiovieira100@gmail.com>
2023-10-19 19:49:48 -0300

    :feat: adicionando rotas

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,57 @@
+import {
+  HomeIcon,
+  ListOrderedIcon,
+  LogInIcon,
+  MenuIcon,
+  PercentIcon,
+  ShoppingCartIcon,
+} from "lucide-react";
+import { Button } from "./button";
+import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
+
+const Header = () => {
+  return (
+    <Card className="flex items-center justify-between p-2">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline">
+            <MenuIcon />
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side={"left"}>
+          <SheetHeader className="text-left text-lg font-semibold">
+            Menu
+          </SheetHeader>
+          <Button variant={"outline"} className="w-full justify-start gap-2">
+            <LogInIcon size={16} />
+            Fazer login
+          </Button>
+          <Button variant={"outline"} className="w-full justify-start gap-2">
+            <HomeIcon size={16} />
+            Inicio
+          </Button>
+          <Button variant={"outline"} className="w-full justify-start gap-2">
+            <PercentIcon size={16} />
+            Ofertas
+          </Button>
+          <Button variant={"outline"} className="w-full justify-start gap-2">
+            <ListOrderedIcon size={16} />
+            Catálogo
+          </Button>
+        </SheetContent>
+      </Sheet>
+
+      <h1 className="text-lg font-semibold">
+        <span className="text-primary">FSW</span> Store
+      </h1>
+
+      <Button size="icon" variant="outline">
+        <ShoppingCartIcon />
+      </Button>
+    </Card>
+  );
+};
+
+export default Header;
